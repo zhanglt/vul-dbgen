d7164ce429d7360b5d19cb0299d9d994209a737d
kitsdk <kitsdk@163.com>
2023-04-26 17:31:21 +0800

    修改中文注释

--- updater/metadata_fetchers.go
+++ updater/metadata_fetchers.go
@@ -12,30 +12,29 @@ type VulnerabilityWithLock struct {
 	Lock sync.Mutex
 }
 
-// MetadataFetcher
+// MetadataFetcher 定义了元数据获取器的接口
 type MetadataFetcher interface {
-	// Load runs right before the Updater calls AddMetadata for each vulnerabilities.
+
+	// Load 在 Updater 调用每个漏洞的 AddMetadata 之前运行。
 	Load(Datastore) error
 
-	// AddMetadata adds metadata to the given database.Vulnerability.
-	// It is expected that the fetcher uses .Lock.Lock() when manipulating the Metadata map.
+	// AddMetadata 向给定的 database.Vulnerability 添加元数据。
+	// 预期该获取器在操作 Metadata map 时使用 .Lock.Lock() 进行加锁。
 	AddMetadata(*VulnerabilityWithLock) error
 
 	LookupMetadata(name string) (string, float64, string, float64, bool)
 	AddAffectedVersion(name string) ([]string, []string, bool)
 	AddCveDate(name string) (time.Time, time.Time, bool)
 
-	// Unload runs right after the Updater finished calling AddMetadata for every vulnerabilities.
+	// Unload 在 Updater 调用完所有漏洞的 AddMetadata 后运行。
 	Unload()
 
-	// Clean deletes any allocated resources.
-	// It is invoked when Clair stops.
+	// Clean 在 Clair 停止时删除任何已分配的资源。
 	Clean()
 }
 
-// RegisterFetcher makes a Fetcher available by the provided name.
-// If Register is called twice with the same name or if driver is nil,
-// it panics.
+// RegisterFetcher 通过提供的名称将 Fetcher 注册为可用的获取器。
+// 如果使用相同的名称多次调用 Register，或者驱动程序为 nil，则会引发 panic。
 func RegisterMetadataFetcher(name string, f MetadataFetcher) {
 	if name == "" {
 		panic("updater: could not register a MetadataFetcher with an empty name")
